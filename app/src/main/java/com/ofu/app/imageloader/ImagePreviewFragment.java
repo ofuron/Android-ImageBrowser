5d91d8fafd8cd2c83db9797d2b66e53a6bfe0b23
Olivier <oliiver@tango.me>
2016-02-14 00:08:32 -0800

    Rescale image in Pager fragment

--- app/src/main/java/com/ofu/app/imageloader/ImagePreviewFragment.java
+++ app/src/main/java/com/ofu/app/imageloader/ImagePreviewFragment.java
@@ -1,14 +1,28 @@
 package com.ofu.app.imageloader;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
+import com.facebook.common.references.CloseableReference;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 import com.ofu.app.imageloader.R;
 
 import java.io.File;
@@ -41,10 +55,26 @@ public class ImagePreviewFragment extends Fragment {
     // Inflate the layout for this fragment
     View rootView =  inflater.inflate(R.layout.fragment_image_preview, container, false);
 
+    Uri uri = Uri.fromFile(new File(mUrl));
+
+    WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+    Display display = wm.getDefaultDisplay();
+    Point size = new Point();
+    display.getSize(size);
+
     SimpleDraweeView image;
     image = (SimpleDraweeView) rootView.findViewById(R.id.image);
-    Uri uri = Uri.fromFile(new File(mUrl));
-    image.setImageURI(uri);
+
+    ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
+        .setResizeOptions(new ResizeOptions(size.x, size.y))
+        .build();
+
+    PipelineDraweeController controller = (PipelineDraweeController)
+        Fresco.newDraweeControllerBuilder()
+            .setImageRequest(request)
+            .setOldController(image.getController())
+            .build();
+    image.setController(controller);
 
     return rootView;
   }
